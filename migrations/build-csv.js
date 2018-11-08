188d19b772620dc6777f5eaa2a9f026dff731e47
artmadeit <this.arthur.email@gmail.com>
2018-11-08 17:10:03 -0500

    fix: didn't read well because commas!!

--- migrations/build-csv.js
+++ migrations/build-csv.js
@@ -3,7 +3,13 @@ const checkpoints = require('./checkpoints');
 const fs = require('fs');
 const path = require('path');
 const data = checkpoints
-  .map(x => [x.identifier, x.state, x.name, x.description, x.type])
+  .map(x => [
+    x.identifier,
+    x.state,
+    `"${x.name}"`,
+    `"${x.description}"`,
+    x.type,
+  ])
   .map(arr => arr.join(','))
   .join('\n');
 
