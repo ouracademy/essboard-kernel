09e445f9615a1f7b50ca9ef5287f8e5eb61511fb
qpdiam <qpdiam@gmail.com>
2018-11-26 19:04:00 -0500

    chore: add enpdoint to origin cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200'
+    origin: ['http://localhost:4200', 'http://localhost:3000'],
   });
   await app.listen(3000);
 }
