0fc5e58de3b902164ce46c980291ea1f7420f44f
artmadeit <this.arthur.email@gmail.com>
2018-12-07 12:34:03 -0500

    chore: warning port config

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,4 @@ WORKDIR /usr/src/app
 COPY ["package.json", "package-lock.json*", "npm-shrinkwrap.json*", "./"]
 RUN npm install
 COPY . .
-EXPOSE 3000
-CMD ["npm", "run", "start:prod"]
+CMD ["npm", "run", "start:prod", '--', '-p', '$PORT']
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.argv[3] || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['http://localhost:4200', 'http://localhost:3000'],
   });
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
